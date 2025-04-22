eac4866c876f4f60ee0242343339731fe126bc62
Veronika Tasova <vtaseva1@gmail.com>
2025-04-22 20:30:59 +0200

    part2

---
+++ main.js
@@ -0,0 +1,21 @@
+// document.querySelector('#input-datetime').value = (new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString()).slice(0, -1).split('.')[0];
+
+// window.scroll({
+// 	top: 0,
+// 	left: 0,
+// 	behavior: 'smooth'
+// });
+
+// const output = document.querySelector("#about1");
+
+// output.addEventListener("active", function () {
+// 	this.style.color = "red";
+// });
+
+
+
+
+
+function myFunction() {
+	document.getElementById("ab");
+}
